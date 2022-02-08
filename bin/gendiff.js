f6f674bd141338b5840c09199133d0e2ccc94721
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-02-08 01:07:32 -0800

    add arguments and format option to bin file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,9 @@ import { program } from 'commander';
 
 program
   .name('gendiff')
+  .argument('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
