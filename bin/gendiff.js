a25442fa76445abeacb818319ee4948ce0f3f0b9
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-02-14 14:03:28 -0800

    add json files processing to genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@
 import { program } from 'commander';
 import genDiff from '../index.js';
 
+/*
 const obj1 = {
   "host": "hexlet.io",
   "timeout": 50,
@@ -15,20 +16,26 @@ const obj2 = {
   "verbose": true,
   "host": "hexlet.io"
 };
+*/
 
 program
   .name('gendiff')
-  // .argument('<filepath1>', 'path to file1')
-  // .argument('<filepath2>', 'path to file2')
+  .argument('<filepath1>', 'path to file1')
+  .argument('<filepath2>', 'path to file2')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
-  .action(() => {
-    console.log(genDiff(obj1, obj2));
+  // .action(() => {
+    // console.log(genDiff(obj1, obj2));
     // console.log('Gvenya', 'Spiral');
-  })
-  // .action((arguments) => {
-    // console.log(arguments[0], arguments[1]);
   // });
+  /*
+  .action((filepath1, filepath2) => {
+    console.log(filepath1, filepath2);
+  });
+  */
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-import { readFileSync } from 'fs';
+import { fstat, readFileSync } from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import genDiffString from './src/genDiffString.js';
 
-const genDiff = (data1, data2) => genDiffString(data1, data2);
+const getFilePath = (fileName) => path.resolve(process.cwd(), fileName);
+
+const getFileContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
+
+const isJSON = (fileName) => path.extname(fileName) === 'json';
+
+const parseData = (fileName) => {
+  const filePath = getFilePath(fileName);
+  const fileContent = getFileContent(filePath);
+  if (isJSON(fileName)) {
+    return JSON.parse(fileContent);
+  }
+};
+
+const genDiff = (file1, file2) => {
+  const file1Path = getFilePath(file1);
+  const file2Path = getFilePath(file2);
+
+  const file1Content = getFileContent(file1Path);
+  const file2Content = getFileContent(file2Path);
+
+  return file2Content;
+}; 
+const genDiffTest = (data1, data2) => genDiffString(data1, data2);
 // JSON.parse of data from files
 
 export default genDiff;
