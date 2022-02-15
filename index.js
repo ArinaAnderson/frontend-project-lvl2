faf3f117cd1a2f33f8e73467b6e5c32c6144e76b
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-02-14 18:16:57 -0800

    fix parseData func

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ import genDiffString from './src/genDiffString.js';
 
 const getFilePath = (fileName) => path.resolve(process.cwd(), '__fixtures__', fileName);
 
-const getFileContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
+const readFileContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
 const isJSON = (fileName) => path.extname(fileName) === '.json';
 
+/*
 const parseData = (fileName) => { // here pass dataright away -- datathat was read from a file
   const filePath = getFilePath(fileName);
   const fileContent = getFileContent(filePath);
@@ -16,24 +17,23 @@ const parseData = (fileName) => { // here pass dataright away -- datathat was re
     return JSON.parse(fileContent);
   }
 };
+*/
+
+const parseData = (fileName, fileData) => {
+  if (isJSON(fileName)) {
+    return JSON.parse(fileData);
+  }
+};
 
 const genDiff = (file1, file2) => {
-  const data1 = parseData(file1);
-  const data2 = parseData(file2);
-  /*
   const file1Path = getFilePath(file1);
   const file2Path = getFilePath(file2);
-
-  const file1Content = getFileContent(file1Path);
-  const file2Content = getFileContent(file2Path);
-
-  return file2Content;
-  */
+  const file1Content = readFileContent(file1Path);
+  const file2Content = readFileContent(file2Path);
+  const data1 = parseData(file1Path, file1Content);
+  const data2 = parseData(file2Path, file2Content);
 
   return genDiffString(data1, data2);
 };
-
-const genDiffTest = (data1, data2) => genDiffString(data1, data2);
-// JSON.parse of data from files
 
 export default genDiff;
