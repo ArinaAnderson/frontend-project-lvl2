eed777af15bd2d3e101ba7e79bd4f3dd15559feb
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-05-05 23:24:56 -0700

    add test for genDiff working flat json files

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,22 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('comparing flat files', () => {
+  const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+  const file1Path = getFixturePath('file1.json');
+  const file2Path = getFixturePath('file2.json');
+  expect(genDiff(file1Path, file2Path)).toEqual(result);
+});
