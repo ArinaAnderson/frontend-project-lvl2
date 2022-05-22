b2f14a3ca329b626593b1b8c742c5376ea8f77a7
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-05-21 22:22:00 -0700

    step5: add new parser and remove old parser

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 import fs from 'fs';
 import path from 'path';
+import defineParser from './src/parsers.js';
 import calcDiff from './src/calcDiff.js';
 import stringify from './src/stringify.js';
 
 const getAbsoluteFilePath = (fileName) => path.resolve(process.cwd(), fileName);
 const readFileContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-const isJSON = (fileName) => path.extname(fileName) === '.json';
-const parseData = (fileName, fileData) => {
-  if (isJSON(fileName)) {
-    return JSON.parse(fileData);
-  }
-  return fileData;
-};
-
 const genDiff = (file1, file2) => {
   const absoluteFile1Path = getAbsoluteFilePath(file1);
   const absoluteFile2Path = getAbsoluteFilePath(file2);
-  
+
   const file1Content = readFileContent(absoluteFile1Path);
   const file2Content = readFileContent(absoluteFile2Path);
-  
-  const obj1 = parseData(file1, file1Content);
-  const obj2 = parseData(file2, file2Content);
+
+  const obj1 = defineParser(absoluteFile1Path)(file1Content);
+  const obj2 = defineParser(absoluteFile2Path)(file2Content);
+
   const diffsObj = calcDiff(obj1, obj2);
+
   const result = stringify(obj1, obj2, diffsObj);
   return result;
 };
