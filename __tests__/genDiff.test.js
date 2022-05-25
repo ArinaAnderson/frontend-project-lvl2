984253d88e34f39c7949f3436bb0d14272eb9a04
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-05-24 18:01:15 -0700

    step6: add use of fixture of txt expected result for flat json files

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,10 +1,13 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
+
 import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
 // console.log(__filename);
 // /Users/marinalex/MY-FOLDERS/Gvenya-prog/2021-2022/HEXLET/Project-2/
@@ -14,20 +17,32 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 // /Users/marinalex/MY-FOLDERS/Gvenya-prog/2021-2022/HEXLET/Project-2/
 // frontend-project-lvl2/__tests__
 
-const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+let expectedResultStylish;
+let expectedResultOriginal;
+/*
+const stylishResultPath = getFixturePath('expect-result-original.txt');
+expectedResultOriginal = readFile(stylishResultPath);
+*/
+
+beforeAll(() => {
+  const originalResultPath = getFixturePath('expect-result-original.txt');
+  expectedResultOriginal = readFile(originalResultPath);
+});
+
 
 test.each([
-  { fileName1: 'file1.json', fileName2: 'file2.json', expected: result },
-  { fileName1: 'file1.yaml', fileName2: 'file2.yaml', expected: result },
-])('comparing flat files', ({ fileName1, fileName2, expected }) => {
+  { fileName1: 'file1A.json', fileName2: 'file2A.json', expected: expectedResultOriginal },
+  // { fileName1: 'file1.yaml', fileName2: 'file2.yaml', expected: expectedResultOriginal },
+])('comparing files in stylish format', ({ fileName1, fileName2, expected }) => {
   const file1Path = getFixturePath(fileName1);
   const file2Path = getFixturePath(fileName2);
   expect(genDiff(file1Path, file2Path)).toEqual(expected);
 });
+
+/*
+test('compare files', () => {
+  const file1Path = getFixturePath('file1A.json');
+  const file2Path = getFixturePath('file2A.json');
+  expect(genDiff(file1Path, file2Path)).toEqual(expectedResultOriginal);
+});
+*/
