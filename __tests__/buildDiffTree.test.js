d278be764e05fd3fc8a349750dff8aba67fe990a
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-05-28 21:16:23 -0700

    step6: add tests for buildDiffTree func

--- __tests__/buildDiffTree.test.js
+++ __tests__/buildDiffTree.test.js
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 
-import calcDiff from '../src/calcDiff.js';
+import buildDiffTree from '../src/buildDiffTree.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,40 +11,18 @@ const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
 let obj1;
 let obj2;
+let expectedResult;
 
 beforeAll(() => {
   const obj1Path = getFixturePath('file1.json');
   const obj2Path = getFixturePath('file2.json');
+  const expectedResultPath = getFixturePath('expect-result-diffTree.json');
   obj1 = JSON.parse(readFile(obj1Path));
   obj2 = JSON.parse(readFile(obj2Path));
+  expectedResult = JSON.parse(readFile(expectedResultPath));
 });
 
-const expectedResult = {
-  common: {
-    follow: 'added',
-    setting1: 'unchanged',
-    setting2: 'deleted',
-    setting3: 'changed',
-    setting4: 'added',
-    setting5: 'added',
-    setting6: {
-      key: 'unchanged',
-      ops: 'added',
-      doge: {
-        wow: 'changed',
-      },
-    },
-  },
-  group1: {
-    baz: 'changed',
-    foo: 'unchanged',
-    nest: 'changed',
-  },
-  group2: 'deleted',
-  group3: 'added',
-};
-
 test('compares nested objects', () => {
-  const actual = calcDiff(obj1, obj2);
+  const actual = buildDiffTree(obj1, obj2);
   expect(actual).toEqual(expectedResult);
 });
