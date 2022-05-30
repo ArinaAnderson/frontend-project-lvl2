8d91fbbb440c017eb9046e14a2ebf8b89fbea678
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-05-30 01:13:12 -0700

    step6: fix tests for genDiff.js

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -9,16 +9,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-// let expectedResultStylish;
-
-const originalResultPath = getFixturePath('expect-result-original.txt');
-const expectedResultOriginal = readFile(originalResultPath);
+const stylishResultPath = getFixturePath('expect-result-stylish.txt');
+const expectedResultStylish = readFile(stylishResultPath);
 
 test.each([
-  { fileName1: 'file1A.json', fileName2: 'file2A.json', expected: expectedResultOriginal },
-  // { fileName1: 'file1.yaml', fileName2: 'file2.yaml', expected: expectedResultOriginal },
+  { fileName1: 'file1.json', fileName2: 'file2.json', expected: expectedResultStylish },
+  { fileName1: 'file1.yaml', fileName2: 'file2.yaml', expected: expectedResultStylish },
 ])('comparing files in stylish format', ({ fileName1, fileName2, expected }) => {
   const file1Path = getFixturePath(fileName1);
   const file2Path = getFixturePath(fileName2);
-  expect(genDiff(file1Path, file2Path)).toEqual(expected);
+  expect(genDiff(file1Path, file2Path)).toBe(expected);
 });
