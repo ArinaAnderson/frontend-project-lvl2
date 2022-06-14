9e56b4ee9605799c9596d79a00bb748313bd7040
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-06-14 16:39:47 -0700

    add buildDiffTreeNode func

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,31 +1,16 @@
 import _ from 'lodash';
 
 const isObject = (val) => _.isObject(val) && !_.isArray(val);
-/*
-const buildDiffTreeNode = (obj1, obj2, key, fn) => {
-  const keyNode = {};
 
-  if (!_.has(obj1, key)) {
-    keyNode.state = 'added';
-    keyNode.val = obj2[key];
-  } else if (!_.has(obj2, key)) {
-    keyNode.state = 'deleted';
-    keyNode.val = obj1[key];
-  } else if (isObject(obj1[key]) && isObject(obj2[key])) {
-    keyNode.state = 'diffSubTree';
-    keyNode.diffSubTree = fn(obj1[key], obj2[key]);
-  } else if (obj1[key] !== obj2[key]) {
-    keyNode.state = 'changed';
-    keyNode.val1 = obj1[key];
-    keyNode.val2 = obj2[key];
-  } else {
-    keyNode.state = 'unchanged';
-    keyNode.val = obj1[key];
-  }
+const buildDiffTreeNode = (key, val, state) => {
+  const diffTreeNode = {};
 
-  return keyNode;
+  diffTreeNode.key = key;
+  diffTreeNode.state = state;
+  diffTreeNode.val = val;
+
+  return diffTreeNode;
 };
-*/
 
 const buildDiffTree = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
@@ -33,58 +18,36 @@ const buildDiffTree = (obj1, obj2) => {
   const keys = _.sortBy(_.union(keys1, keys2));
 
   const diffTree = keys.map((key) => {
-    // console.log(key, 'BASYAA');
     const elem = [];
-    const keyNode = {};
-    if (!_.has(obj1, key)) {
-      keyNode.key = key;
-      keyNode.state = 'added';
-      keyNode.val = obj2[key];
-      elem.push(keyNode);
-    } else if (!_.has(obj2, key)) {
-      keyNode.key = key;
-      keyNode.state = 'deleted';
-      keyNode.val = obj1[key];
-      elem.push(keyNode);
-    } else if (isObject(obj1[key]) && isObject(obj2[key])) {
-      keyNode.key = key;
-      keyNode.state = 'diffSubTree';
-      keyNode.diffSubTree = buildDiffTree(obj1[key], obj2[key]);
-      elem.push(keyNode);
-    } else if (obj1[key] !== obj2[key]) {
-      const keyNode1 = {};
-      const keyNode2 = {};
-      keyNode1.key = key;
-      keyNode1.state = 'deleted';
-      keyNode1.val = obj1[key];
-      elem.push(keyNode1);
 
-      keyNode2.key = key;
-      keyNode2.state = 'added';
-      keyNode2.val = obj2[key];
-      elem.push(keyNode2);
+    if (!_.has(obj1, key)) {
+      const diffTreeNode = buildDiffTreeNode(key, obj2[key], 'added');
+      elem.push(diffTreeNode);
+    } else if (!_.has(obj2, key)) {
+      const diffTreeNode = buildDiffTreeNode(key, obj1[key], 'deleted');
+      elem.push(diffTreeNode);
+    } else if (isObject(obj1[key]) && isObject(obj2[key])) {
+      const diffTreeNode = buildDiffTreeNode(
+        key,
+        buildDiffTree(obj1[key], obj2[key]),
+        'diffSubTree',
+      );
+      elem.push(diffTreeNode);
+    } else if (obj1[key] !== obj2[key]) {
+      const diffTreeNode1 = buildDiffTreeNode(key, obj1[key], 'deleted');
+      elem.push(diffTreeNode1);
+
+      const diffTreeNode2 = buildDiffTreeNode(key, obj2[key], 'added');
+      elem.push(diffTreeNode2);
     } else {
-      keyNode.key = key;
-      keyNode.state = 'unchanged';
-      keyNode.val = obj1[key];
-      elem.push(keyNode);
+      const diffTreeNode = buildDiffTreeNode(key, obj1[key], 'unchanged');
+      elem.push(diffTreeNode);
     }
+
     return elem;
   });
 
   return diffTree.flat();
-  /*
-  const keys1 = _.keys(obj1);
-  const keys2 = _.keys(obj2);
-  const keys = _.sortBy(_.union(keys1, keys2));
-
-  const diffTree = keys.reduce((acc, key) => {
-    const keyNode = buildDiffTreeNode(obj1, obj2, key, buildDiffTree);
-    return { ...acc, [key]: keyNode };
-  }, {});
-
-  return diffTree;
-  */
 };
 
 export default buildDiffTree;
