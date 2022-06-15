1a74068149ed604f95f5a03eb4760d65bfad21fd
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-06-14 17:13:32 -0700

    add processObjectsKey func for  buildDiffTree map

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -17,12 +17,32 @@ const getTreeNodeState = (obj1, obj2) => {
 };
 */
 
+const processObjectsKey = (obj1, obj2, key, fn) => {
+  if (!_.has(obj1, key)) {
+    return buildTreeNode(key, obj2[key], 'added');
+  }
+  if (!_.has(obj2, key)) {
+    return buildTreeNode(key, obj1[key], 'deleted');
+  }
+  if (isObject(obj1[key]) && isObject(obj2[key])) {
+    return buildTreeNode(key, fn(obj1[key], obj2[key]), 'diffSubTree');
+  }
+  if (obj1[key] !== obj2[key]) {
+    return [
+      buildTreeNode(key, obj1[key], 'deleted'),
+      buildTreeNode(key, obj2[key], 'added'),
+    ];
+  }
+  return buildTreeNode(key, obj1[key], 'unchanged');
+};
+
 const buildDiffTree = (obj1, obj2) => {
   const keys1 = _.keys(obj1);
   const keys2 = _.keys(obj2);
   const keys = _.sortBy(_.union(keys1, keys2));
 
-  const diffTree = keys.map((key) => {
+  const diffTree = keys.map((key) => processObjectsKey(obj1, obj2, key, buildDiffTree));
+  /*
     if (!_.has(obj1, key)) {
       return buildTreeNode(key, obj2[key], 'added');
     }
@@ -40,7 +60,7 @@ const buildDiffTree = (obj1, obj2) => {
     }
     return buildTreeNode(key, obj1[key], 'unchanged');
   });
-
+  */
   return diffTree.flat();
 };
 
