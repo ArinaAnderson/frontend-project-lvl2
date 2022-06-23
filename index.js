df935af25692c6a3cdd0000172eab7168faf0aa6
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-06-22 23:11:13 -0700

    set format parametre for genDiff func

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import formatStylish from './src/formatStylish.js';
 const getAbsoluteFilePath = (fileName) => path.resolve(process.cwd(), fileName);
 const readFileContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-const genDiff = (file1, file2) => { // format = 'stylish') => {
+const genDiff = (file1, file2, format = 'stylish') => { // format = 'stylish') => {
   const absoluteFile1Path = getAbsoluteFilePath(file1);
   const absoluteFile2Path = getAbsoluteFilePath(file2);
 
