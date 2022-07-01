456eaedea7785f8bea9b0ac4b483381882ecd5d6
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-06-30 21:48:56 -0700

    place logic of genDiff from index.js to src/index.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-import defineParser from './src/parsers.js';
-import buildDiffTree from './src/buildDiffTree.js';
-import getFormat from './src/formatters/index.js';
-
-const getAbsoluteFilePath = (fileName) => path.resolve(process.cwd(), fileName);
-const readFileContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
-
-const genDiff = (file1, file2, formatName = 'stylish') => {
-  const absoluteFile1Path = getAbsoluteFilePath(file1);
-  const absoluteFile2Path = getAbsoluteFilePath(file2);
-
-  const file1Content = readFileContent(absoluteFile1Path);
-  const file2Content = readFileContent(absoluteFile2Path);
-
-  const obj1 = defineParser(absoluteFile1Path)(file1Content);
-  const obj2 = defineParser(absoluteFile2Path)(file2Content);
-
-  const diffsObj = buildDiffTree(obj1, obj2);
-
-  const format = getFormat(formatName);
-
-  const result = format(diffsObj);
-  return result;
-};
+import genDiff from './src/index.js';
 
 export default genDiff;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import defineParser from './parsers.js';
+import buildDiffTree from './buildDiffTree.js';
+import getFormat from './formatters/index.js';
+
+const getAbsoluteFilePath = (fileName) => path.resolve(process.cwd(), fileName);
+const readFileContent = (filePath) => fs.readFileSync(filePath, 'utf-8');
+
+const genDiff = (file1, file2, formatName = 'stylish') => {
+  const absoluteFile1Path = getAbsoluteFilePath(file1);
+  const absoluteFile2Path = getAbsoluteFilePath(file2);
+
+  const file1Content = readFileContent(absoluteFile1Path);
+  const file2Content = readFileContent(absoluteFile2Path);
+
+  const obj1 = defineParser(absoluteFile1Path)(file1Content);
+  const obj2 = defineParser(absoluteFile2Path)(file2Content);
+
+  const diffsObj = buildDiffTree(obj1, obj2);
+
+  const format = getFormat(formatName);
+
+  const result = format(diffsObj);
+  return result;
+};
+
+export default genDiff;
