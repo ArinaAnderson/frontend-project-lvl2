0761a4531661053fb82f3420224e2a97a23615c0
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-06-30 21:58:44 -0700

    replace isObject func with lodash func isPlainObject in buildDiffTree

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,18 +1,11 @@
 import _ from 'lodash';
 
-const isObject = (val) => _.isObject(val) && !_.isArray(val);
-
 const buildTreeNode = (key, val, state) => {
   const diffTreeNode = {
     key,
     state,
     val,
   };
-  /*
-  diffTreeNode.key = key;
-  diffTreeNode.state = state;
-  diffTreeNode.val = val;
-  */
 
   return diffTreeNode;
 };
@@ -24,7 +17,7 @@ const processObjectsKey = (obj1, obj2, key, fn) => {
   if (!_.has(obj2, key)) {
     return buildTreeNode(key, obj1[key], 'deleted');
   }
-  if (isObject(obj1[key]) && isObject(obj2[key])) {
+  if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
     return buildTreeNode(key, fn(obj1[key], obj2[key]), 'diffSubTree');
   }
   if (obj1[key] !== obj2[key]) {
