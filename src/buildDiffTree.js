7f2882e653ce6658ef02c4d88c0f5397f96d09c3
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-07-07 22:54:16 -0700

    change the way updated value is processed

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
 
 const buildTreeNode = (key, val, state) => {
+  const keyNameForValues = state === 'updated' ? 'vals' : 'val';
   const diffTreeNode = {
     key,
     state,
     val,
+    // [keyNameForValues]: val,
+    // val,
   };
 
   return diffTreeNode;
@@ -21,10 +24,16 @@ const processObjectsKey = (obj1, obj2, key, buildDiffTreeFunc) => {
     return buildTreeNode(key, buildDiffTreeFunc(obj1[key], obj2[key]), 'diffSubTree');
   }
   if (obj1[key] !== obj2[key]) {
+    return buildTreeNode(key, {
+      'deleted': obj1[key],
+      'added': obj2[key],
+    }, 'updated');
+    /*
     return [
       buildTreeNode(key, obj1[key], 'deleted'),
       buildTreeNode(key, obj2[key], 'added'),
     ];
+    */
   }
   return buildTreeNode(key, obj1[key], 'unchanged');
 };
@@ -40,3 +49,64 @@ const buildDiffTree = (obj1, obj2) => {
 };
 
 export default buildDiffTree;
+
+const objAA = {
+  "common": {
+    "setting1": "Value 1",
+    "setting2": 200,
+    "setting3": true,
+    "setting6": {
+      "key": "value",
+      "doge": {
+        "wow": "",
+      }
+    }
+  },
+  "group1": {
+    "baz": "bas",
+    "foo": "bar",
+    "nest": {
+      "key": "value"
+    }
+  },
+  "group2": {
+    "abc": 12345,
+    "deep": {
+      "id": 45
+    }
+  }
+};
+
+const objBB = {
+  "common": {
+    "follow": false,
+    "setting1": "Value 1",
+    "setting3": null,
+    "setting4": "blah blah",
+    "setting5": {
+      "key5": "value5"
+    },
+    "setting6": {
+      "key": "value",
+      "ops": "vops",
+      "doge": {
+        "wow": "so much",
+      }
+    }
+  },
+  "group1": {
+    "foo": "bar",
+    "baz": "bars",
+    "nest": "str"
+  },
+  "group3": {
+    "deep": {
+      "id": {
+        "number": 45
+      }
+    },
+    "fee": 100500
+  }
+};
+
+console.log(JSON.stringify(buildDiffTree(objAA, objBB), null, '  '));
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -37,32 +37,177 @@ const defineMargin = (indentSize, replacer, markerLength = 2) => {
   return margin;
 };
 
+/*
 const defineIndent = (state, indentSize, replacer) => {
   const lineMarker = defineMarker(state, replacer);
   const lineMargin = defineMargin(indentSize, replacer, lineMarker.length);
 
   return `${lineMargin}${lineMarker}`;
 };
+*/
+
+const defineIndent = (margin, marker) => `${margin}${marker}`;
 
 const createLine = (key, value, indent) => `${indent}${key}: ${value}`;
 
 const stylish = (diffsTree, indentBase = 4, replacer = ' ') => {
   const iter = (diffs, depth) => {
     const lines = diffs.map((node) => {
-      const lineKey = node.key;
-      const lineIndent = defineIndent(node.state, depth * indentBase, replacer);
-
-      if (node.state === 'diffSubTree') {
-        return createLine(lineKey, iter(node.val, depth + 1), lineIndent);
+      const { key, val, state } = node;
+      const lineMargin = defineMargin(depth * indentBase, replacer);
+      
+      if (state === 'updated') {
+        const deletedValIndent = defineIndent(lineMargin, defineMarker('deleted', replacer));
+        const addedValIndent = defineIndent(lineMargin, defineMarker('added', replacer));
+        return [
+          createLine(key, stringify(val.deleted, indentBase, depth + 1, replacer), deletedValIndent),
+          createLine(key, stringify(val.added, indentBase, depth + 1, replacer),addedValIndent),
+        ];
       }
 
-      return createLine(lineKey, stringify(node.val, indentBase, depth + 1, replacer), lineIndent);
+      const lineMarker = defineMarker(state, replacer);
+      const lineIndent = defineIndent(lineMargin, lineMarker);
+
+
+      if (state === 'diffSubTree') {
+        return createLine(key, iter(val, depth + 1), lineIndent);
+      }
+
+      return createLine(key, stringify(val, indentBase, depth + 1, replacer), lineIndent);
     });
 
-    return `{\n${lines.join('\n')}\n${replacer.repeat(depth * indentBase - indentBase)}}`;
+    return `{\n${lines.flat().join('\n')}\n${replacer.repeat(depth * indentBase - indentBase)}}`;
+    // return `{\n${lines.join('\n')}\n${replacer.repeat(depth * indentBase - indentBase)}}`;
   };
 
   return iter(diffsTree, 1);
 };
 
 export default stylish;
+
+const diffs = [
+  {
+    "key": "common",
+    "state": "diffSubTree",
+    "val": [
+      {
+        "key": "follow",
+        "state": "added",
+        "val": false
+      },
+      {
+        "key": "setting1",
+        "state": "unchanged",
+        "val": "Value 1"
+      },
+      {
+        "key": "setting2",
+        "state": "deleted",
+        "val": 200
+      },
+      {
+        "key": "setting3",
+        "state": "updated",
+        "val": {
+          "deleted": true,
+          "added": null
+        }
+      },
+      {
+        "key": "setting4",
+        "state": "added",
+        "val": "blah blah"
+      },
+      {
+        "key": "setting5",
+        "state": "added",
+        "val": {
+          "key5": "value5"
+        }
+      },
+      {
+        "key": "setting6",
+        "state": "diffSubTree",
+        "val": [
+          {
+            "key": "doge",
+            "state": "diffSubTree",
+            "val": [
+              {
+                "key": "wow",
+                "state": "updated",
+                "val": {
+                  "deleted": "",
+                  "added": "so much"
+                }
+              }
+            ]
+          },
+          {
+            "key": "key",
+            "state": "unchanged",
+            "val": "value"
+          },
+          {
+            "key": "ops",
+            "state": "added",
+            "val": "vops"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "key": "group1",
+    "state": "diffSubTree",
+    "val": [
+      {
+        "key": "baz",
+        "state": "updated",
+        "val": {
+          "deleted": "bas",
+          "added": "bars"
+        }
+      },
+      {
+        "key": "foo",
+        "state": "unchanged",
+        "val": "bar"
+      },
+      {
+        "key": "nest",
+        "state": "updated",
+        "val": {
+          "deleted": {
+            "key": "value"
+          },
+          "added": "str"
+        }
+      }
+    ]
+  },
+  {
+    "key": "group2",
+    "state": "deleted",
+    "val": {
+      "abc": 12345,
+      "deep": {
+        "id": 45
+      }
+    }
+  },
+  {
+    "key": "group3",
+    "state": "added",
+    "val": {
+      "deep": {
+        "id": {
+          "number": 45
+        }
+      },
+      "fee": 100500
+    }
+  }
+];
+
+console.log(stylish(diffs));
