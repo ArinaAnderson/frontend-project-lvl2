64240513ac8714663dab2e1bde2b5879fcf22828
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-07-13 22:33:00 -0700

    refactor stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -14,46 +14,36 @@ const stringify = (value, indentBase = 4, depth = 1, replacer = ' ') => {
   return iter(value, depth * indentBase);
 };
 
-const replacer = ' ';
-
-const statesToMarkers = {
-  added: '+',
-  deleted: '-',
-  unchanged: replacer,
-  diffSubTree: replacer,
-};
-
-const defineIndent = (indentSize, markerLength = 1) => {
-  const indent = `${replacer.repeat(indentSize - markerLength - replacer.length)}`;
+const defineIndent = (indentSize, replacer, markerLength = 2) => {
+  const indent = `${replacer.repeat(indentSize - markerLength)}`;
   return indent;
 };
 
-const createLine = (key, value, indent, marker) => `${indent}${marker}${replacer}${key}: ${value}`;
+const createLine = (key, value, indent, marker) => `${indent}${marker}${key}: ${value}`;
 
-const turnNodesToLines = (nodes, defineLineVal, indent) => {
-  const lines = nodes.map((node) => {
-    if (node.state === 'updated') {
-      return turnNodesToLines(node.val, defineLineVal, indent);
-    }
-
-    const marker = statesToMarkers[node.state];
-
-    const line = createLine(node.key, defineLineVal(node), indent, marker);
-    return line;
-  });
-  return lines;
-};
-
-const stylish = (diffsTree, indentBase = 4) => {
+const stylish = (diffsTree, indentBase = 4, replacer = ' ') => {
   const iter = (diffs, depth) => {
-    const indent = defineIndent(depth * indentBase);
-    const getLineContent = (node) => {
-      if (node.state === 'diffSubTree') {
-        return iter(node.val, depth + 1);
+    const indent = defineIndent(depth * indentBase, replacer);
+    const lines = diffs.map((node) => {
+      const { key, val, state } = node;
+      switch (state) {
+        case 'updated':
+          return [
+            createLine(key, stringify(val[0].val, indentBase, depth + 1), indent, `-${replacer}`),
+            createLine(key, stringify(val[1].val, indentBase, depth + 1), indent, `+${replacer}`),
+          ];
+        case 'deleted':
+          return createLine(key, stringify(val, indentBase, depth + 1), indent, `-${replacer}`);
+        case 'added':
+          return createLine(key, stringify(val, indentBase, depth + 1), indent, `+${replacer}`);
+        case 'unchanged':
+          return createLine(key, stringify(val, indentBase, depth + 1), indent, `${replacer}${replacer}`);
+        case 'diffSubTree':
+          return createLine(key, iter(val, depth + 1), indent, `${replacer}${replacer}`);
+        default:
+          throw new Error(`Unknown state: '${state}'!`);
       }
-      return stringify(node.val, indentBase, depth + 1);
-    };
-    const lines = turnNodesToLines(diffs, getLineContent, indent);
+    });
 
     return `{\n${lines.flat().join('\n')}\n${replacer.repeat(depth * indentBase - indentBase)}}`;
   };
